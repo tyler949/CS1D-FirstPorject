16d91fdd1b7b12b0ac633b50e722e79ad699f368
tyler949 <tyler949@yahoo.com>
2015-02-19 18:05:10 -0800

    Update PropagateVector.cpp

--- PropagateVector.cpp
+++ PropagateVector.cpp
@@ -84,22 +84,3 @@ void PropagateVector(vector<WineryClass>& passedFirstVec)
     
     inFile.close();
 }
-
-<<<<<<< HEAD
-=======
-//change to make 
-// for (int i = 0; i < winesOffered; i++)
-// {
-//     getline(inFile, wineTypeName);
-//     inFile >> wineYear;
-//     inFile.ignore(1000, '\n');
-//     inFile >> wineCost;
-//     inFile.ignore(1000, '\n');
-//     temp.setYear(wineYear);
-//     temp.setCost(wineCost);
-//     temp.setName(wineTypeName);
-
-//     tempWineTypeVector.pushback(temp);
-
-// }
->>>>>>> FETCH_HEAD
