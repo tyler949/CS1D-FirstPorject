8109cc79ca06180c68610000cbc1f49400d5b605
Stormbiter <Stormbiter123@yahoo.com>
2015-03-10 20:08:01 -0700

    Create shortestdistance.cpp

---
+++ shortestdistance.cpp
@@ -0,0 +1,30 @@
+#include "WineryClass.h"
+void shortestDistance(vector<WineryClass> winvec, int start, int num, vector<WineryClass> &tripvec)
+{
+	double smallest = 9999;
+	int smallestPntr;
+	int k = start;
+	tripvec.push_back(winvec.at(start));
+	winvec.at(start).vist();
+
+	for(int i = start; i <= num; i++)
+	{
+		WineryClass temp = winvec.at(k);
+		for(int count = 0; count < winvec.size(); count++)
+		{
+			if(smallest > temp.getDistance(count)  && !(winvec.at(count).getVisted()))
+			{
+				cout << "here" << endl;
+				smallest = temp.getDistance(count);
+				smallestPntr = count;
+			}
+		}
+
+		cout << endl << endl;
+		tripvec.push_back(winvec.at(smallestPntr));
+		winvec.at(smallestPntr).vist();
+		k = smallestPntr;
+		smallest = 999999;
+	}
+
+}
