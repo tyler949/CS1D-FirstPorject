0438ee71c2c527fef7427ca2b7733a8d3231d964
Stormbiter <Stormbiter123@yahoo.com>
2015-03-12 16:24:01 -0700

    Create updateVector.cpp

---
+++ updateVector.cpp
@@ -0,0 +1,22 @@
+#include "WineryClass.h"
+void updateVector(vector<WineryClass> orginal, vector<WineryClass> &newVec)
+{
+	int key;
+	for(int i = 0; i < newVec.size(); i++)
+	{
+		newVec.at(i).distanceClear();
+	}
+	for(int i = 0; i < newVec.size(); i++)
+	{
+		key = newVec.at(i).getWineryNumber() -1;
+		for(int j = 0; j < newVec.size(); j++)
+		{
+
+			newVec.at(j).addDistance(orginal.at(newVec.at(j).getWineryNumber() - 1).getDistance(key));
+		}
+	}
+	for(int i = 0; i < newVec.size(); i ++)
+	{
+		newVec.at(i).setWineryNumber((i + 1));
+	}
+}
