a3e5fd59cc1e70ff639ea96253731f3bb661eae4
Stormbiter <Stormbiter123@yahoo.com>
2015-03-12 16:25:48 -0700

    Update WineryClass.cpp

--- WineryClass.cpp
+++ WineryClass.cpp
@@ -1,189 +1,178 @@
 
-
 #include "WineryClass.h"
-
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
 WineryClass::WineryClass()
 {
-    name = "blank name";
-    wineryNumber = 0;
-    numberOfWinerys = 0;
-    distanceVec.reserve(30);
-    milesToVilla = 0;
-    winesOffered = 0;
-    winesVec.reserve(10);
+name = "blank name";
+wineryNumber = 0;
+numberOfWinerys = 0;
+distanceVec.reserve(0);
+milesToVilla = 0;
+winesOffered = 0;
+winesVec.reserve(0);
+visted = false;
 }
-
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
 WineryClass::~WineryClass()
 {
-    
 }
-
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
 ////non-default constructor
 //WineryClass::WineryClass(string passedName, int passedWineryNumber,
-//                         int passedNumberOfWinerys,
-//                         vector<double>& passedDistanceVec,
-//                         double passedMilesToVilla, int passedWinesOffered,
-//                         vector<wineType>& passedWineTypeVector)
+// int passedNumberOfWinerys,
+// vector<double>& passedDistanceVec,
+// double passedMilesToVilla, int passedWinesOffered,
+// vector<wineType>& passedWineTypeVector)
 //{
-//    distanceVec().reserve(30);
-//    winesVec().reserve(10);
-//    
-//    name = passedName;
-//    wineryNumber = passedWineryNumber;
-//    numberOfWinerys = passedNumberOfWinerys;
-//    
-//    for (int i = 0; i < sizeof(passedDistanceVec); i++)
-//    {
-//        distanceVec().push_back(passedDistanceVec.at(i));
+// distanceVec().reserve(30);
+// winesVec().reserve(10);
 //
-//    }
+// name = passedName;
+// wineryNumber = passedWineryNumber;
+// numberOfWinerys = passedNumberOfWinerys;
 //
-//    milesToVilla = passedMilesToVilla;
-//    winesOffered = passedWinesOffered;
-//    
-//    for (int i = 0; i < sizeof(passedWineTypeVector); i++)
-//    {
-//        winesVec().at(i) = passedWineTypeVector.at(i);
-//    }
-//    
+// for (int i = 0; i < sizeof(passedDistanceVec); i++)
+// {
+// distanceVec().push_back(passedDistanceVec.at(i));
+//
+// }
+//
+// milesToVilla = passedMilesToVilla;
+// winesOffered = passedWinesOffered;
+//
+// for (int i = 0; i < sizeof(passedWineTypeVector); i++)
+// {
+// winesVec().at(i) = passedWineTypeVector.at(i);
+// }
+//
 //}
-
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 //accessors
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-
 string WineryClass::getWineryName()
 {
-    return name;
+return name;
 }
-
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
 int WineryClass::getWineryNumber()
 {
-    return wineryNumber;
+return wineryNumber;
 }
-
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
 int WineryClass::getNumberOfWinerys()
 {
-    return numberOfWinerys;
+return numberOfWinerys;
 }
-
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
 double WineryClass::getDistance(int x)
 {
-    return distanceVec.at(x);
+return distanceVec.at(x);
 }
-
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
 double WineryClass::getMilesToVilla()
 {
-    return milesToVilla;
+return milesToVilla;
 }
-
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
 int WineryClass::getWinesOffered()
 {
-    return winesOffered;
+return winesOffered;
 }
-
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
 string WineryClass::getWineName(int x)
 {
-    return winesVec.at(x).getName();
+return winesVec.at(x).getName();
 }
-
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
 int WineryClass::getWineYear(int x)
 {
-    return winesVec.at(x).getYear();
+return winesVec.at(x).getYear();
 }
-
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
 double WineryClass::getWineCost(int x)
 {
-    return winesVec.at(x).getCost();
+return winesVec.at(x).getCost();
 }
-
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 //mutators~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
 void WineryClass::setWineryName(string passedName)
 {
-    name = passedName;
+name = passedName;
 }
-
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
 void WineryClass::setWineryNumber(int passedNumber)
 {
-    wineryNumber = passedNumber;
+wineryNumber = passedNumber;
 }
-
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
 void WineryClass::setNumberOfWinerys(int passedNumber)
 {
-    numberOfWinerys = passedNumber;
+numberOfWinerys = passedNumber;
 }
-
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
 void WineryClass::setDistance(int x, double passedDistance)
 {
-    distanceVec.at(x) = passedDistance;
+distanceVec.at(x) = passedDistance;
 }
-
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
 void WineryClass::setMilesToVilla(double passedMiles)
 {
-    milesToVilla = passedMiles;
+milesToVilla = passedMiles;
 }
-
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
 void WineryClass::setWinesOffered(int passedWinesOffered)
 {
-    winesOffered = passedWinesOffered;
+winesOffered = passedWinesOffered;
 }
-
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 //used for setting a specific wineType node within the wineries
 void WineryClass::setWineName(int x, string passedName)
 {
-    winesVec.at(x).setName(passedName);
+winesVec.at(x).setName(passedName);
 }
-
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
 void WineryClass::setWineYear(int x, int passedWineYear)
 {
-    winesVec.at(x).setYear(passedWineYear);
+winesVec.at(x).setYear(passedWineYear);
 }
-
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
+void WineryClass:: distanceClear()
+{
+	distanceVec.clear();
+}
 void WineryClass::setWineCost(int x, double passedWineCost)
 {
-    winesVec.at(x).setCost(passedWineCost);
+winesVec.at(x).setCost(passedWineCost);
+}
+void WineryClass:: addDistance(double x)
+{
+	distanceVec.push_back(x);
 }
 
+void WineryClass:: addWine(wineType wine)
+{
+	winesVec.push_back(wine);
+}
+void WineryClass:: clear()
+{
+	distanceVec.clear();
+    winesVec.clear();
+}
+bool WineryClass:: getVisted()
+{
+	return visted;
+}
+void WineryClass:: vist()
+{
+	visted = true;
+}
+int WineryClass:: numWines()
+{
+	return	winesVec.size();
+}
+int WineryClass::	numDistances()
+{
+	return distanceVec.size();
+}
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
